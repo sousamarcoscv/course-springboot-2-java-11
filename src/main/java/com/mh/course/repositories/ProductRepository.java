cd0e2ffe8c765971db6e62f973eca8278224ab92
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-29 12:49:34 -0300

    Product

---
+++ src/main/java/com/mh/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.mh.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mh.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
