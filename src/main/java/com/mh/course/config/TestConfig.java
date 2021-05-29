48eba0c45f228528574e3df6583a94d23d36e51a
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-29 19:59:02 -0300

    Payment, one-to-one association

--- src/main/java/com/mh/course/config/TestConfig.java
+++ src/main/java/com/mh/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.mh.course.entities.Category;
 import com.mh.course.entities.Order;
 import com.mh.course.entities.OrderItem;
+import com.mh.course.entities.Payment;
 import com.mh.course.entities.Product;
 import com.mh.course.entities.User;
 import com.mh.course.entities.enums.OrderStatus;
@@ -79,6 +80,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T22:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 
 	}
