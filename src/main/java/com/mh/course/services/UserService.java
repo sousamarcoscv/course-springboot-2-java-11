923c7f7e6791f1d31737147a67210976d79d435b
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-30 11:48:31 -0300

    Exception handling - update

--- src/main/java/com/mh/course/services/UserService.java
+++ src/main/java/com/mh/course/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mh.course.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +44,13 @@ public class UserService {
 	}
 
 	public User uptade(Long id, User obj) {
-		Optional<User> entity = repository.findById(id);
-		updateData(entity.get(), obj);
-		return repository.save(entity.get());
+		try {
+			Optional<User> entity = repository.findById(id);
+			updateData(entity.get(), obj);
+			return repository.save(entity.get());
+		} catch (NoSuchElementException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
