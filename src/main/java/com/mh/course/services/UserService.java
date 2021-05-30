7642f101d663c4f0f7357dcd294aee1f7b9130bf
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-30 11:38:40 -0300

    Exception handling - delete

--- src/main/java/com/mh/course/services/UserService.java
+++ src/main/java/com/mh/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mh.course.entities.User;
 import com.mh.course.repositories.UserRepository;
+import com.mh.course.services.exceptions.DatabaseException;
 import com.mh.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -19,24 +22,29 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	
+
 	public void delele(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User uptade(Long id, User obj) {
-		Optional <User> entity = repository.findById(id);
-		updateData(entity.get(),obj);
+		Optional<User> entity = repository.findById(id);
+		updateData(entity.get(), obj);
 		return repository.save(entity.get());
 	}
 
@@ -44,6 +52,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
